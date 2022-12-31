2ec0b516251865fdd4e2f80feecc1442e5c9f89c
adomaitisc <caadomaitis@gmail.com>
2022-12-31 01:39:50 -0300

    updated website with animations

--- inspect-gpt-next/pages/index.tsx
+++ inspect-gpt-next/pages/index.tsx
@@ -1,11 +1,12 @@
+import { motion } from "framer-motion";
+
 import Head from "next/head";
 import Image from "next/image";
 
 import bg from "../public/background.svg";
 import logo from "../public/inspect-gpt.svg";
+import extension from "../public/extension.svg";
 import chrome from "../public/chrome.png";
-import demo from "../public/demo.png";
-import Link from "next/link";
 
 export default function Home() {
   return (
@@ -13,7 +14,7 @@ export default function Home() {
       <Image
         src={bg}
         alt="background-image"
-        className="fixed top-0 left-0 w-screen -z-10"
+        className="absolute top-0 left-0 w-screen -z-[1] opacity-80 blur-lg"
       />
       <Head>
         <title>Create Next App</title>
@@ -22,41 +23,50 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="w-full h-full flex flex-col items-center">
-        <header className="w-full h-20 flex justify-between items-center px-32 box-border">
+        <header className="w-full px-32 py-6">
           <Image src={logo} alt={""} />
-          <Link
-            href="/dashboard"
-            className="bg-gpt-600 text-gpt-100 text-base font-medium rounded-full px-6 py-2 shadow-sm"
-          >
-            Sign In
-          </Link>
         </header>
         <section className="flex flex-col gap-10 py-36 items-center">
-          <h1 className="text-gpt-100 text-6xl font-bold">InspectGPT</h1>
-          <p className="text-gpt-100/80 text-lg font-medium max-w-xl text-center">
+          <motion.h1
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ delay: 0.4, duration: 0.3 }}
+            className="text-white text-7xl font-bold"
+          >
+            InspectGPT
+          </motion.h1>
+          <motion.p
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ delay: 0.8, duration: 0.3 }}
+            className="text-white/80 text-xl font-medium max-w-xl text-center"
+          >
             Our extension makes it simple to find GPT-generated text on any web
             page. Just install it and let it scan the page for you.
-          </p>
-          <div className="flex gap-6">
-            <button className="flex items-center bg-gpt-100 text-gpt-500 text-base font-semibold rounded-full px-6 py-2 shadow-sm">
-              Get extension
-            </button>
-            <button className="text-gpt-100 text-base font-medium px-6 py-2 underline">
-              Learn more
-            </button>
-          </div>
+          </motion.p>
+          <motion.button
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ delay: 1.2, duration: 0.3 }}
+            className="flex gap-2 items-center justify-center bg-gradient-to-br from-blue-800 to-blue-600 text-white text-lg font-semibold rounded-2xl px-6 py-2 shadow-sm"
+          >
+            <Image src={chrome} alt={"chrome logo"} height={20} />
+            Install Extension
+          </motion.button>
         </section>
-        <section className="pb-36">
+        <motion.div
+          initial={{ opacity: 0, y: 100, rotateZ: 10, scale: 0.8 }}
+          animate={{ opacity: 1, y: 0, rotateZ: 0, scale: 1 }}
+          transition={{ delay: 2, duration: 0.3 }}
+          className="pb-36"
+        >
           <Image
-            src={demo}
+            src={extension}
             alt={""}
-            className="max-w-4xl border-2 border-white rounded-[1.5rem] shadow-md"
+            className="max-w-4xl rounded-[20px] shadow-md border border-zinc-900/50"
           />
-        </section>
+        </motion.div>
         <footer className="py-12 flex flex-col items-center">
-          <aside className="text-base rounded-full mb-6 ml-6 font-bold px-6 py-2 text-gpt-100 bg-gpt-300/60">
-            12 Wonderful People are using InspectGPT
-          </aside>
           <a
             rel="noopener noreferrer"
             target="_blank"
