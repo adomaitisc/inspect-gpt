12a311d5d92e9c0f26d4af42992ce45e4a982726
adomaitisc <caadomaitis@gmail.com>
2023-01-02 17:29:34 -0300

    website finished

--- inspect-gpt-next/tailwind.config.js
+++ inspect-gpt-next/tailwind.config.js
@@ -16,6 +16,25 @@ module.exports = {
           600: "#183929",
         },
       },
+      animation: {
+        blob: "blob 7s infinite",
+      },
+      keyframes: {
+        blob: {
+          "0%": {
+            transform: "translate(0px, 0px) scale(1)",
+          },
+          "33%": {
+            transform: "translate(30px, -50px) scale(1.1)",
+          },
+          "66%": {
+            transform: "translate(-20px, 20px) scale(0.9)",
+          },
+          "100%": {
+            transform: "tranlate(0px, 0px) scale(1)",
+          },
+        },
+      },
     },
   },
   plugins: [],
