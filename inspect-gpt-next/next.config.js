8f7a1fc98fe2db98114583947e633560a5f41ff4
adomaitisc <caadomaitis@gmail.com>
2023-01-03 19:38:13 -0300

    xss headers

--- inspect-gpt-next/next.config.js
+++ inspect-gpt-next/next.config.js
@@ -1,30 +1,38 @@
-const { createSecureHeaders } = require("next-secure-headers");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   images: {
     domains: ["www.datocms-assets.com"],
   },
-};
-
-module.exports = {
+  // Adding policies:
   async headers() {
     return [
       {
         source: "/(.*)",
-        headers: createSecureHeaders({
-          contentSecurityPolicy: {
-            directives: {
-              defaultSrc: ["'self'", "https://inspectgpt.com"],
-              styleSrc: ["'self'"],
-              imgSrc: ["'self'", "'unsafe-inline'"],
-              baseUri: "self",
-              formAction: "self",
-              frameAncestors: true,
-            },
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
           },
-        }),
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self' 'http://inspectgpt.com'; image-src 'www.datocms-assets.com'; script-src 'self'; font-src 'self' 'https://fonts.googleapis.com'",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Permissions-Policy",
+            value: "camera=(); battery=(); geolocation=(); microphone=()",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "origin-when-cross-origin",
+          },
+        ],
       },
     ];
   },
