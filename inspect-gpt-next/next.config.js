7c997d90b44b2f193301b26a01fac23aee35055b
adomaitisc <caadomaitis@gmail.com>
2023-01-03 19:31:44 -0300

    trying to use some xss protection

--- inspect-gpt-next/next.config.js
+++ inspect-gpt-next/next.config.js
@@ -1,8 +1,32 @@
+const { createSecureHeaders } = require("next-secure-headers");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["www.datocms-assets.com"],
+  },
+};
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: createSecureHeaders({
+          contentSecurityPolicy: {
+            directives: {
+              defaultSrc: ["'self'", "https://inspectgpt.com"],
+              styleSrc: ["'self'"],
+              imgSrc: ["'self'", "'unsafe-inline'"],
+              baseUri: "self",
+              formAction: "self",
+              frameAncestors: true,
+            },
+          },
+        }),
+      },
+    ];
   },
 };
 
