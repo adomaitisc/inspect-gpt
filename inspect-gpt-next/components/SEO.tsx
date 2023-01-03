a9e762dd515a15096f59a0d6aa2ab4c1ffce3c4f
adomaitisc <caadomaitis@gmail.com>
2023-01-03 20:30:58 -0300

    SEO complete with favicon

--- inspect-gpt-next/components/SEO.tsx
+++ inspect-gpt-next/components/SEO.tsx
@@ -31,7 +31,27 @@ export default function SEO() {
         name="twitter:image"
         content="https://github.com/adomaitisc/inspect-gpt/blob/main/inspect-gpt-next/public/og-image.png?raw=true"
       />
-      <link rel="icon" href="/favicon.ico" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#333333" />
+      <meta name="msapplication-TileColor" content="#2b5797" />
+      <meta name="theme-color" content="#333333" />
     </Head>
   );
 }
