601d39d2545ac1df47ad01be0be01804cec89030
adomaitisc <caadomaitis@gmail.com>
2023-01-03 17:14:06 -0300

    FIXED SCROLL OVERFLOW

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Our extension makes it simple to find GPT-generated text on any web page. Just i
 - [x] Loading state for extension.
 - [ ] Extension debug on more websites
 - [ ] Publish on Chrome store
-- [ ] Fix scroll overflow
+- [x] Fix scroll overflow
 - [ ] Add XSS prevention on the backend
 - [ ] Add account feature to save scanned pages
 - [x] Add star on GitHub button
--- inspect-gpt-next/pages/index.tsx
+++ inspect-gpt-next/pages/index.tsx
@@ -14,15 +14,17 @@ import extension from "../public/extension.svg";
 
 export default function Home() {
   return (
-    <div className="w-screen overflow-x-hidden">
-      <div
-        id="blob-one"
-        className="absolute top-0 left-0 h-96 w-96 bg-purple-300 rounded-full blur-[128px] -z-10"
-      />
-      <div
-        id="blob-two"
-        className="absolute bottom-0 right-0 h-96 w-96 bg-blue-300 rounded-full blur-[128px] -z-10"
-      />
+    <div className="w-full overflow-x-hidden">
+      <div className="fixed w-full top-0 bottom-0 -z-20">
+        <div
+          id="blob-one"
+          className="absolute top-0 left-0 h-96 w-96 bg-purple-300 rounded-full blur-[128px]"
+        />
+        <div
+          id="blob-two"
+          className="absolute bottom-0 right-0 h-96 w-96 bg-blue-300 rounded-full blur-[128px]"
+        />
+      </div>
       <Image
         src={noise}
         alt={""}
