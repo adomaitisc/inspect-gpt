abda32cbc77e03f88fa11d070e06b508b7d3de71
adomaitisc <caadomaitis@gmail.com>
2023-01-04 04:26:06 -0300

    didnt like the loader + my website link on footer

--- inspect-gpt-next/components/Footer.tsx
+++ inspect-gpt-next/components/Footer.tsx
@@ -2,9 +2,14 @@ export default function Footer() {
   const currentYear = new Date().getFullYear();
   return (
     <footer className="pb-12 flex flex-col items-center">
-      <p className="text-white/60 font-medium text-sm pt-4">
+      <a
+        href="https://adomaitisc.com"
+        rel="noreferrer"
+        target="_blank"
+        className="text-white/60 font-medium text-sm pt-4 cursor-pointer"
+      >
         {currentYear} © Cauã Adomaitis
-      </p>
+      </a>
     </footer>
   );
 }
