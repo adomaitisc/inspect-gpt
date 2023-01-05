bf51ece82e8fcd9bedf044f93e24fb2106c15056
adomaitisc <caadomaitis@gmail.com>
2023-01-05 16:14:57 -0300

    Option collapsable is working nicely

--- inspect-gpt-next/components/Option.tsx
+++ inspect-gpt-next/components/Option.tsx
@@ -1,4 +1,6 @@
 import { motion } from "framer-motion";
+import Image from "next/image";
+import chevron from "../public/chevron.svg";
 
 export default function Option({
   setOpen,
@@ -11,20 +13,52 @@ export default function Option({
   title: string;
   content: string;
 }) {
+  let borderTop;
+  let borderTopRadius;
+  let borderBottomRadius;
+  let rotate;
+
+  if (open) {
+    borderTopRadius = "1rem";
+    borderBottomRadius = "0";
+    rotate = "90deg";
+    borderTop = "none";
+  } else {
+    borderTopRadius = "1rem";
+    borderBottomRadius = "1rem";
+    rotate = "270deg";
+    borderTop = "2px";
+  }
+
   return (
     <>
       <div
+        style={{
+          borderTopRightRadius: borderTopRadius,
+          borderTopLeftRadius: borderTopRadius,
+          borderBottomRightRadius: borderBottomRadius,
+          borderBottomLeftRadius: borderBottomRadius,
+        }}
         onClick={() => setOpen(!open)}
-        className="text-xl block font-bold text-purple-600 w-full py-4 px-6 rounded-3xl border-2 border-zinc-300 bg-zinc-300/20 backdrop-blur-lg"
+        className="mt-4 w-3/4 py-2 px-6 border-2 border-zinc-300 bg-zinc-200/40 backdrop-blur-lg cursor-pointer"
       >
-        {title}
+        <h2 className="flex gap-2 items-center justify-centerw-full text-xl font-bold">
+          <Image style={{ rotate }} src={chevron} alt="chevron" /> {title}
+        </h2>
       </div>
       {open && (
         <motion.div
           initial={{ opacity: 0, translateY: -30 }}
           animate={{ opacity: 1, translateY: 0 }}
           transition={{ duration: 0.2 }}
-          className="py-4 px-6 rounded-3xl border-2 border-zinc-300 bg-zinc-300/20 backdrop-blur-lg"
+          style={{
+            borderTop,
+            borderTopRightRadius: borderBottomRadius,
+            borderTopLeftRadius: borderBottomRadius,
+            borderBottomRightRadius: borderTopRadius,
+            borderBottomLeftRadius: borderTopRadius,
+          }}
+          className="w-3/4 py-4 px-10 border-2 border-zinc-300 bg-zinc-300/20 backdrop-blur-lg"
         >
           {content}
         </motion.div>
--- inspect-gpt-next/pages/support.tsx
+++ inspect-gpt-next/pages/support.tsx
@@ -7,7 +7,10 @@ import Option from "../components/Option";
 import { useState } from "react";
 
 export default function Support() {
-  const [collapseOpen, setCollapseOpen] = useState(false);
+  const [firstCollapseOpen, setFirstCollapseOpen] = useState(false);
+  const [secondCollapseOpen, setSecondCollapseOpen] = useState(false);
+  const [thirdCollapseOpen, setThirdCollapseOpen] = useState(false);
+  const [fourthCollapseOpen, setFourthCollapseOpen] = useState(false);
 
   return (
     <>
@@ -23,34 +26,34 @@ export default function Support() {
           <Header />
           <section className="flex flex-col gap-10 py-20 items-center">
             <Help />
-            <div className="flex flex-col gap-5 items-center max-w-4xl w-3/4">
+            <div className="flex flex-col items-center w-[90vw]">
               <Option
-                setOpen={setCollapseOpen}
-                open={collapseOpen}
+                setOpen={setFirstCollapseOpen}
+                open={firstCollapseOpen}
                 title={"How do I reset my password?"}
                 content={
                   "To reset your password, click on the 'Forgot Password' link on the login page and follow the prompts to reset your password."
                 }
               />
               <Option
-                setOpen={setCollapseOpen}
-                open={collapseOpen}
+                setOpen={setSecondCollapseOpen}
+                open={secondCollapseOpen}
                 title={"How do I cancel my account?"}
                 content={
                   "To cancel your account, please contact our support team through the support form or by emailing [support email address]. Please provide your account information and the reason for cancelling."
                 }
               />
               <Option
-                setOpen={setCollapseOpen}
-                open={collapseOpen}
+                setOpen={setThirdCollapseOpen}
+                open={thirdCollapseOpen}
                 title={"How do I update my payment information?"}
                 content={
                   "A: To update your payment information, log in to your account and go to the 'Billing' section. From there, you can update your payment method."
                 }
               />
               <Option
-                setOpen={setCollapseOpen}
-                open={collapseOpen}
+                setOpen={setFourthCollapseOpen}
+                open={fourthCollapseOpen}
                 title={"How do I contact support?"}
                 content={
                   "You can contact our support team through the support form on our website or by emailing [support email address]. Our team is available to assist you Monday through Friday, 9am to 5pm EST."
