9ca376486fe33e1607c9b57d6f20d4d29c481831
adomaitisc <caadomaitis@gmail.com>
2023-01-12 01:10:42 -0500

    it was tough to fix the extension - it is on its way now, after a ton of thinking

--- inspect-gpt-extension/popup.js
+++ inspect-gpt-extension/popup.js
@@ -1,21 +1,25 @@
 // popup.js
 
-// Send message to content script to get paragraphs and chunks
+// Send message to content script to get paragraphs and collections
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
   chrome.tabs.sendMessage(
     tabs[0].id,
     { message: "get_paragraphs_and_chunks" },
     function (response) {
+      // Get paragraphs and collections from response
       let paragraphs = response.paragraphs;
-      let chunks = response.chunks;
+      let collections = response.collections;
 
       // Display loading screen
       document.getElementById("loading-screen").style.display = "block";
 
-      // Make API requests for each paragraph and chunk
-      let promises = [];
+      // Make API requests for each paragraph and collection
+      let paragraphPromises = [];
+      let collectionPromises = [];
+
+      // Appending to paragraphPromises[]
       for (let i = 0; i < paragraphs.length; i++) {
-        promises.push(
+        paragraphPromises.push(
           fetch(`http://localhost:3000/api/paragraph-scan`, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
@@ -23,35 +27,41 @@ chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
           })
         );
       }
-      for (let i = 0; i < chunks.length; i++) {
-        promises.push(
+
+      // Appending to collectionPromises[]
+      for (let i = 0; i < collections.length; i++) {
+        collectionPromises.push(
           fetch(`http://localhost:3000/api/paragraph-scan`, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ text: chunks[i] }),
+            body: JSON.stringify({ text: collections[i] }),
           })
         );
       }
 
-      // Wait for all API requests to complete
-      Promise.all(promises)
+      // Execute and wait for all the requests to return
+      Promise.all(paragraphPromises)
         .then((responses) => Promise.all(responses.map((r) => r.json())))
-        .then((data) => {
-          // Display paragraphs and chunks with their probabilities
-          let results = document.getElementById("results");
-          for (let i = 0; i < data.length; i++) {
-            results.innerHTML += `<p>${data[i].text} - Probability: ${data[i].probability}</p>`;
-          }
-
-          // Hide loading screen
-          document.getElementById("loading-screen").style.display = "none";
-        })
-        .catch((error) => {
-          console.error("Error:", error);
-
-          // Hide loading screen
-          document.getElementById("loading-screen").style.display = "none";
-        });
+        .then((data) => handleParagraphData(data))
+        .catch((error) => handleError(error));
     }
   );
 });
+
+function handleParagraphData(data) {
+  // Display paragraphs with their probabilities
+  let results = document.getElementById("results");
+  for (let i = 0; i < data.length; i++) {
+    results.innerHTML += `<p>${data[i].text} - Probability: ${data[i].probability}</p>`;
+  }
+  // Hide loading screen
+  document.getElementById("loading-screen").style.display = "none";
+}
+
+function handleError(error) {
+  // Show error message
+  document.getElementById("errors").style.display = "block";
+  document.getElementById("errors").innerHTML = error;
+  // Hide loading screen
+  document.getElementById("loading-screen").style.display = "none";
+}
